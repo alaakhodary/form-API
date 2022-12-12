b074ffc734fc39fdd2e2366d5cfa0dc7492eeac7
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2022-12-12 12:14:28 +0200

    handel data to showing in the browser

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let displayForm = document.querySelector(".add-icon .icon-plus");
 let form = document.querySelector(".form-all");
+const myForm = document.querySelector(".All-1");
+const todoName = document.querySelector(".input-1");
+const todoEmail = document.querySelector(".input-2");
+
 displayForm.addEventListener("click", function () {
   form.style.display = "block";
 });
@@ -9,12 +13,8 @@ xhr.onreadystatechange = () => {
   if (xhr.readyState === 4 && xhr.status === 200) {
     // console.log(JSON.parse(xhr.response));
     let data = JSON.parse(xhr.response);
-    data.forEach((user) => {
-      const markup = `<div class="box">
-                        <h1 class="heading">${user.name}</h1>
-                        <p class="paragraph">${user.email}</P>
-                      </div>`;
-      document.querySelector(".box").insertAdjacentHTML("afterend", markup);
+    data.map(function (user) {
+      handelTodo(user);
     });
   } else if (xhr.readyState === 4 && xhr.status !== 200) {
     throw Error("sth went wrong");
@@ -23,17 +23,31 @@ xhr.onreadystatechange = () => {
 xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
 xhr.send();
 
-const myForm = document.querySelector(".All-1");
 myForm.addEventListener("submit", async function (e) {
   e.preventDefault();
-  const formData = new FormData(myForm).entries();
   const response = await fetch("https://jsonplaceholder.typicode.com/users", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(Object.fromEntries(formData)),
+    body: JSON.stringify({
+      name: todoName.value,
+      email: todoEmail.value,
+    }),
   });
   const result = await response.json();
-  console.log(result);
+  handelTodo(result);
 });
+
+function handelTodo(todo) {
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+  let box = document.querySelector(".box");
+  box.classList.value = "box";
+  h1.classList.value = "heading";
+  p.classList.value = "paragraph";
+  h1.textContent = `${todo.name}`;
+  p.textContent = `${todo.email}`;
+  box.appendChild(h1);
+  box.appendChild(p);
+}
